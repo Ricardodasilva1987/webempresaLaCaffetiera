4ece18d6c045c201c1744799a388c8a3b2e8ffa8
Ricardo Da Silva <dasilvaricardog@gmail.com>
2022-10-11 20:16:32 -0300

    cambio

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Post,Category
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 # Create your views here.
 
+
 def blog(request):
-    posts =Post.objects.all()
-    return render(request,'blog/blog.html',{'posts':posts})
+    posts = Post.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts})
 
 
-def category (request, category_id):
+def category(request, category_id):
     #category = Category.objects.get(id=category_id)
-    category= get_object_or_404(Category,id =category_id) #se usa este metodo que es un get si trae algo de la base o sino genera el error 404
+    # se usa este metodo que es un get si trae algo de la base o sino genera el error 404
+    category = get_object_or_404(Category, id=category_id)
     #posts = Post.objects.filter(categories=category)
-    
-    return render(request,'blog/category.html',{'category':category})
+
+    return render(request, 'blog/category.html', {'category': category})
+
+# pruebaaa
